0eed707d3485aa3b1bf2e0e0069e8f5c459e03ea
Kryndach <kryndach.ios@gmail.com>
2017-06-27 13:07:43 +0600

    fix UIDevice force try

--- Project-iOS/XLProjectName/XLProjectName/Helpers/Extensions/UIDevice.swift
+++ Project-iOS/XLProjectName/XLProjectName/Helpers/Extensions/UIDevice.swift
@@ -11,16 +11,25 @@ import UIKit
 import KeychainAccess
 
 extension UIDevice {
-    
+
     fileprivate static let keychainKey = "device_id"
     fileprivate static let keychain = Keychain(service: UIApplication.bundleIdentifier)
-    
+
     static var uniqueId: String {
-        if try! keychain.contains(keychainKey) {
-            return try! keychain.get(keychainKey)!
+        do {
+            if try keychain.contains(keychainKey) {
+                guard let id = try keychain.get(keychainKey) else {
+                    return ""
+                }
+                return id
+            }
+            guard let newDeviceId = UIDevice.current.identifierForVendor?.uuidString else {
+                return ""
+            }
+            try keychain.set(newDeviceId, key: keychainKey)
+            return newDeviceId
+        } catch {
+            return ""
         }
-        let newDeviceId = UIDevice.current.identifierForVendor!.uuidString
-        try! keychain.set(newDeviceId, key: keychainKey)
-        return newDeviceId
     }
 }
